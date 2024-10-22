1424520e1fe2f2603b8b154a94173c9d89627992
radekaadek <radekaadek@gmail.com>
2024-10-22 18:13:46 +0200

    small padding fixes

--- src/components/Familiada.tsx
+++ src/components/Familiada.tsx
@@ -22,11 +22,11 @@ const Familiada = () => {
                 }
             ],
             <div>
-                <span className="p-3 text-xl text-center" style={{color: "#f3f6f9"}}>A desktop app created with my friend</span>
+                <span className="p-3 text-xl text-center" style={{color: "#f3f6f9", paddingRight: "6px"}}>A desktop app created with my friend</span>
                 <Link href="github.com/xKRISTOFx" passHref legacyBehavior>
                 <span className="text-xl text-center text-blue-500 hover:cursor-pointer">xKRISTOFx</span>
                 </Link>
-                <span className="p-3 text-xl text-center" style={{color: "#f3f6f9"}}>
+                <span className="p-3 text-xl text-center" style={{color: "#f3f6f9", paddingLeft: "6px"}}>
                     that creates an interface for conducting the polish TV show &quot;Familiada&quot; in Python using Pygame and the Tkinter library.
                 </span>
             </div>
