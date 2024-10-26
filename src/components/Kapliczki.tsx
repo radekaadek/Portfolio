108f176d2afdc0b4fd774ffcdfbc87ff1fb784db
radekaadek <radekaadek@gmail.com>
2024-10-26 20:21:59 +0200

    kapliczki

--- src/components/Kapliczki.tsx
+++ src/components/Kapliczki.tsx
@@ -6,8 +6,30 @@ const Kapliczki = () => {
         Project(
             "Kleszczów Shrines",
             [
+                {
+                    src: "/shrines.png",
+                    alt: "shrines",
+                    width: "600",
+                    height: "375",
+                    className: ""
+                },
+                {
+                    src: "/kapliczka.png",
+                    alt: "add_shrine",
+                    width: "600",
+                    height: "375",
+                    className: ""
+                }
             ],
-            <div/>
+            <div className="w-full">
+                <span className="p-3 text-xl text-center" style={{color: "#f3f6f9", paddingRight: "6px"}}>Interactive web app created with friends @</span>
+                <Link href="github.com/xKRISTOFx" passHref legacyBehavior>
+                <span className="text-xl text-center text-blue-500 hover:cursor-pointer">Technical School in Kleszczów</span>
+                </Link>
+                <span className="p-3 text-xl text-center" style={{color: "#f3f6f9", paddingLeft: "6px"}}>
+                     that displays catholic shrines in Kleszczów. The app also includes a module where users can report new shrines and contact maintainers.
+                </span>
+            </div>
         )
     )
 }
